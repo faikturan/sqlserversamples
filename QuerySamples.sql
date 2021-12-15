154b7254c55b4e0f147dda81d8da40b81094c85d
Faik TURAN <faikturan@users.noreply.github.com>
2021-12-15 21:10:30 +0300

    Update QuerySamples.sql

--- QuerySamples.sql
+++ QuerySamples.sql
@@ -4,20 +4,17 @@ select * from production.brands
 select * from production.categories
 
 
-
---Hangi marka ve kategoride hangi ürünler var. 
---Yýllara göre kategori satýþlarý
+--Hangi marka ve kategoride hangi Ã¼rÃ¼nler var. 
+--YÄ±llara gÃ¶re kategori satÄ±ÅŸlarÄ±
 
 
 select 
-PP.category_id,SUM(PP.list_price) AS [TOPLAM SATÝS], PP.model_year
+PP.category_id,SUM(PP.list_price) AS [TOPLAM SATÄ°S], PP.model_year
 from production.products PP LEFT JOIN production.brands PB
 ON PP.brand_id=PB.brand_id 
 LEFT JOIN production.categories PC
 ON PC.category_id=PP.category_id
 group by PP.model_year,PP.category_id
-
-
 
 
 select Pb.brand_name , PC.category_name, pp.product_name
@@ -26,3 +23,20 @@ from production.products PP LEFT JOIN production.brands PB
 ON PP.brand_id=PB.brand_id 
 LEFT JOIN production.categories PC
 ON PC.category_id=PP.category_id
+
+--Hangi store da hangi Ã§alÄ±ÅŸan hangi sipariÅŸi almÄ±ÅŸ
+select * from sales.stores
+select * from sales.staffs
+select * from sales.orders
+select * from sales.order_items
+
+select
+SS.store_name,
+(SS2.first_name + ' ' + SS2.last_name) as FullName,
+SO.order_id
+
+from sales.orders SO
+LEFT JOIN sales.stores SS
+ON SO.store_id = SS.store_id
+LEFT JOIN sales.staffs SS2
+ON SS2.staff_id =SO.staff_id
